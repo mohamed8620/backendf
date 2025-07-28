15416db5e379afa4164f1b79ed8cc9000fe556a0
hp <a.ashraf225556@gmail.com>
2025-07-28 23:07:35 +0300

    Update env

--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ Route::post('/register', [AuthController::class, 'register']);
 
 Route::post('/login', [AuthController::class, 'login']);
 
+
 Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
 
 Route::middleware('auth:sanctum')->get('/dashboard', [AuthController::class, 'dashboard']);
